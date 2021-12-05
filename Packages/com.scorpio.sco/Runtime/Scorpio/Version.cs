3406b199adf7684023bd1a7b416d3e86ce708ff9
while <qingfeng346@fengyuezhu.com>
2021-12-05 11:28:44 +0800

    update

---
+++ Packages/com.scorpio.sco/Runtime/Scorpio/Version.cs
@@ -0,0 +1,7 @@
+//github : https://github.com/qingfeng346/Scorpio-CSharp
+namespace Scorpio {
+    public static class Version {
+        public const string version = "2.3.5";
+        public const string date = "2021-11-29";
+    }
+}
