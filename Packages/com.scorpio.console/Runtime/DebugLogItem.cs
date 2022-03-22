ec483dcb9718f1def152dce6a89f496ed2622307
linyuan.yang <linyuan.yang@centurygame.com>
2022-03-22 20:24:00 +0800

    Scorpio Console

---
+++ Packages/com.scorpio.console/Runtime/DebugLogItem.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scorpio.Console {
+    public class DebugLogItem : MonoBehaviour, IVirtualView {
+        public RectTransform rectTransform { get; private set; }
+        public Sprite spriteInfo, spriteWarn, spriteError;
+        public Image imageLogType;
+        public Text textLogStr;
+        private DebugLogEntry entry;
+        void Awake() {
+            rectTransform = transform as RectTransform;
+            ScorpioDebugUtil.RegisterClick(gameObject, this.OnClick);
+        }
+        public void SetDataContext(object data) {
+            var entry = data as DebugLogEntry;
+            SetLogEntry(entry);
+        }
+        public void SetParent(Transform parent) {
+            rectTransform.SetParent(parent);
+            rectTransform.transform.localScale = Vector3.one;
+        }
+        public void SetLogEntry(DebugLogEntry entry) {
+            this.entry = entry;
+            this.textLogStr.text = entry.LogString;
+            switch (entry.logType) {
+                case DebugLogType.Error:
+                    imageLogType.sprite = spriteError;
+                    break;
+                case DebugLogType.Warn:
+                    imageLogType.sprite = spriteWarn;
+                    break;
+                case DebugLogType.Info:
+                    imageLogType.sprite = spriteInfo;
+                    break;
+            }
+        }
+        void OnClick() {
+            ScorpioConsole.Instance.ShowLogInfo(entry);
+        }
+    }
+}
---
+++ Packages/com.scorpio.console/Runtime/ScorpioDebugUtil.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections.Generic;
+public class ScorpioDebugUtil {
+
+    public static GameObject GetGameObject(UnityEngine.Object obj) { 
+        return obj is Component ? (obj as Component).gameObject : obj as GameObject; 
+    }
+    public static GameObject FindChild(UnityEngine.Object obj, string str) {
+        var gameObject = GetGameObject(obj);
+        if (gameObject == null) { return null; }
+        if (string.IsNullOrEmpty(str)) return gameObject;
+        var transform = gameObject.transform.Find(str);
+        return transform == null ? null : transform.gameObject;
+    }
+    public static T FindChild<T>(UnityEngine.Object obj, string str) where T : Component {
+        var gameObject = FindChild(obj, str);
+        return gameObject == null ? null : gameObject.GetComponent<T>();
+    }
+    public static T GetComponent<T>(UnityEngine.Object obj) where T : Component {
+        var gameObject = GetGameObject(obj);
+        if (gameObject == null) { return null; }
+        return gameObject.GetComponent<T>();
+    }
+    public static void RegisterClick(UnityEngine.Object obj, UnityAction action) {
+        RegisterClick(obj, null, action);
+    }
+    public static void RegisterClick(UnityEngine.Object obj, string path, UnityAction action) {
+        var button = FindChild<Button>(obj, path);
+        if (button == null) { return; }
+        var buttonEvent = new Button.ButtonClickedEvent();
+        buttonEvent.AddListener(action);
+        button.onClick = buttonEvent;
+    }
+    public static void RegisterSubmit(UnityEngine.Object obj, UnityAction<string> submit) {
+        RegisterSubmit(obj, null, submit);
+    }
+    public static void RegisterSubmit(UnityEngine.Object obj, string path, UnityAction<string> submit) {
+        var input = FindChild<InputField>(obj, path);
+        if (input == null) { return; }
+        var inputEvent = new InputField.SubmitEvent();
+        inputEvent.AddListener(submit);
+        input.onEndEdit = inputEvent;
+    }
+}
