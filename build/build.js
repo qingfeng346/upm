8629e7a03cd87acdfff0305218e69d023f85f285
linyuan.yang <linyuan.yang@centurygame.com>
2022-05-06 19:22:00 +0800

    update

--- build/build.js
+++ build/build.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const os = require('os')
 const path = require('path')
-const { spawn } = require('child_process')
+const { spawn, exec, execSync } = require('child_process')
 async function main() {
     let args = process.argv.splice(2)
     let cwd = process.cwd()
@@ -10,20 +10,20 @@ async function main() {
     console.log(`cwd:${cwd}  name:${name} version:${version}`)
     rmdir("./tmp")
     if (name == "sco") {
-        await exec("git", cwd, ["clone", "-b", `v${version}`, "https://github.com/qingfeng346/Scorpio-CSharp.git", "./tmp/sco"])
+        await execSpawn("git", cwd, ["clone", "-b", `v${version}`, "https://github.com/qingfeng346/Scorpio-CSharp.git", "./tmp/sco"])
     } else if (name == "scov") {
-        await exec("git", cwd, ["clone", "-b", `v${version}`, "https://github.com/qingfeng346/ScorpioConversion.git", "./tmp/scov"])
+        await execSpawn("git", cwd, ["clone", "-b", `v${version}`, "https://github.com/qingfeng346/ScorpioConversion.git", "./tmp/scov"])
     }
     try {
         let unityVersion = "2019.4.15f1"
         let unityPath = os.platform() == "win32" ? `D:/Program Files/${unityVersion}/Editor/Unity.exe` : `/Applications/${unityVersion}/Unity.app/Contents/MacOS/Unity`
-        await exec(unityPath, null, ["-batchmode", "-quit", "-projectPath", path.dirname(cwd), "-logFile", `${cwd}/unity.log`, "-executeMethod", "Command.Execute", "--args", "-name", name, "-version", version])
+        await execSpawn(unityPath, null, ["-batchmode", "-quit", "-projectPath", path.dirname(cwd), "-logFile", `${cwd}/unity.log`, "-executeMethod", "Command.Execute", "--args", "-name", name, "-version", version])
     } catch (e) {
         console.log(e)
     }
     rmdir("./tmp")
 }
-function exec(command, cwd, args) {
+function execSpawn(command, cwd, args) {
     return new Promise((resolve) => {
         let sp = spawn(command, args, { cwd: cwd })
         let stdout = ""
