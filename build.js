84e6f45fcdc4c3d110108d6d9a2f8857b15b945d
linyuan.yang <linyuan.yang@centurygame.com>
2022-05-06 18:54:54 +0800

    update

--- build.js
+++ build.js
@@ -4,7 +4,8 @@ async function main() {
     let args = process.argv.splice(2)
     let name = args[0]
     let version = args[1]
-    let unityPath = `D:/Program Files/2019.4.15f1/Editor/Unity.exe`
+    let unityVersion = "2019.4.15f1"
+    let unityPath = os.platform() == "win32" ? `D:/Program Files/${unityVersion}/Editor/Unity.exe` : `/Applications/${unityVersion}/Unity.app/Contents/MacOS/Unity`
     rmdir("./tmp")
     if (name == "sco") {
         await exec("git", ["clone", "-b", `v${version}`, "https://github.com/qingfeng346/Scorpio-CSharp.git", "./tmp/sco"])
