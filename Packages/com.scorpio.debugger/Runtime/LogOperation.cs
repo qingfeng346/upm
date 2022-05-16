2ccececab41654d1b0110c7bb02b5211a8f75320
linyuan.yang <linyuan.yang@centurygame.com>
2022-05-16 17:01:28 +0800

    update

---
+++ Packages/com.scorpio.debugger/Runtime/LogOperation.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Scorpio.Debugger {
+    /// <summary>
+    /// 日志操作
+    /// </summary>
+    public class LogOperation {
+        public string label;
+        public Action<LogEntry> action;
+    }
+}
