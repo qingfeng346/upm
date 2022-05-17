f5792963425685a8c127e54560fdd8c7c233d553
linyuan.yang <linyuan.yang@centurygame.com>
2022-05-17 14:47:11 +0800

    update

--- Packages/com.scorpio.debugger/Runtime/ScorpioDebuggerWindow.cs
+++ Packages/com.scorpio.debugger/Runtime/ScorpioDebuggerWindow.cs
@@ -6,6 +6,8 @@ public class ScorpioDebuggerWindow : MonoBehaviour
     public RectTransform tabs;
     public RectTransform items;
     public Text hideText;
+    public GameObject minimize;
+    public GameObject maximize;
     public bool visiable = true;
     public void OnClickShowHide() {
         SetVisiable(!visiable);
@@ -22,5 +24,13 @@ public class ScorpioDebuggerWindow : MonoBehaviour
             items.offsetMin = new Vector2(0, 0);
             hideText.text = "▷";
         }
+    }
+    public void OnClickMinimize() {
+        minimize.SetActive(true);
+        maximize.SetActive(false);
+    }
+    public void OnClickMaximize() {
+        minimize.SetActive(false);
+        maximize.SetActive(true);
     }
 }
