e503afe04055e7bef0374a94e8112f78d3e07209
while <qingfeng346@fengyuezhu.com>
2022-09-15 18:12:57 +0800

    update

---
+++ Assets/Editor/ResourceCommand.cs
@@ -0,0 +1,14 @@
+﻿using Scorpio.Resource;
+using Scorpio.Resource.Editor;
+using Scorpio.Unity.Command;
+using UnityEditor;
+[InitializeOnLoad]
+public class ResourceCommand {
+    static ResourceCommand() {
+        CommandBuild.AddCommand("BuildBlueprints", BuildBlueprints);
+    }
+    static void BuildBlueprints() {
+        var builder = new AssetBundleBuilder<FileList>(AssetDatabase.LoadAssetAtPath<BuilderSetting>("Assets/Commands/BuilderSetting.asset"));
+        builder.BuildBlueprints(null);
+    }
+}
